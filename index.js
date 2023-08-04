d4532a3f7fbf0c5753e87b16f2672ecc0d962636
Guilherme dos Santos <gs92dev@gmail.com>
2023-08-04 18:01:27 -0500

    Created news letter

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var current = 0,
-    slides = document.getElementsByClassName("slides"); 
+  slides = document.getElementsByClassName("slides");
 
-setInterval(function() {
+setInterval(function () {
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.opacity = 0;
   }
-  current = (current != slides.length - 1) ? current + 1 : 0;
+  current = current != slides.length - 1 ? current + 1 : 0;
   slides[current].style.opacity = 1;
 }, 5000);
+
+let letter = document.getElementById("letter");
+let close = document.querySelector("[data-letter=close]");
+console.log(close);
+window.onload = function () {
+  letter.classList.remove("none");
+};
+close.onclick = function () {
+  letter.classList.add("none")
+}
